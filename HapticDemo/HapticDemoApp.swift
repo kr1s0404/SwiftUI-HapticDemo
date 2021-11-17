e3a1136bec715aa9e2cf1eadb58ee5648bc1f52a
kr1s0404 <tylerlolz0404@gmail.com>
2021-11-17 10:10:02 +0800

    init commit

---
+++ HapticDemo/HapticDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticDemoApp.swift
+//  HapticDemo
+//
+//  Created by Kris on 11/17/21.
+//
+
+import SwiftUI
+
+@main
+struct HapticDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
